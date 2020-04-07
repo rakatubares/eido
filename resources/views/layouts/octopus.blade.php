cacf87256b683e80f8b40682fcd2f0966a689fe2
rakatubares <rakatubares@gmail.com>
2020-04-07 21:54:00 +0700

    make logout form

--- resources/views/layouts/octopus.blade.php
+++ resources/views/layouts/octopus.blade.php
@@ -248,7 +248,9 @@
 									<a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i class="fa fa-lock"></i> Lock Screen</a>
 								</li>
 								<li>
-									<a role="menuitem" tabindex="-1" href="pages-signin.html"><i class="fa fa-power-off"></i> Logout</a>
+								{!! Form::open(['id' => 'logoutForm', 'method' => 'POST', 'route' => 'logout']) !!}
+									<a id="logoutBtn" role="menuitem" tabindex="-1" href="#"><i class="fa fa-power-off"></i> Logout</a>
+								{!! Form::close() !!}
 								</li>
 							</ul>
 						</div>
@@ -435,6 +437,12 @@
 		<script src="{{ asset('js/octopus/theme.init.js') }}"></script>
 		
 		<!-- Page specific -->
+		<script>
+			$('#logoutBtn').on('click', function(e) {
+				e.preventDefault();
+				$('#logoutForm').submit();
+			});
+		</script>
 		@yield('pagescript')
 
 	</body>
