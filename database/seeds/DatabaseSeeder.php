8ed721983920ad42687bf57236940d881a1c7e36
rakatubares <rakatubares@gmail.com>
2020-04-07 09:42:10 +0700

    compile seeder files

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call([
+            PermissionTableSeeder::class,
+            CreateAdminUserSeeder::class,
+            DimJenisImportirSeeder::class,
+            DimRekomendasiSeeder::class,
+            DimStatusSeeder::class,
+        ]);
     }
 }
